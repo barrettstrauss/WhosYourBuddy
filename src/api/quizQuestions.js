6b21cf1b669a4a9d86c068c71d342cd80082d113
Barrett Strauss <barrettstrauss@gmail.com>
2017-12-13 20:36:00 -0600

    questions

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,115 @@
+var quizQuestions = [
+  {
+      question: "Do you like pancakes?",
+      answers: [
+        {
+            type: "Joey",
+            content: "Hell Yeah"
+        },
+        {
+            type: "Mack",
+            content: "Joey Sucks"
+        },
+        {
+            type: "Brett",
+            content: "I'm on a strict no fat diet"
+        },
+        {
+            type: "Becca",
+            content: "Yes, but thats a secret"
+        },
+        {
+            type: "Sarah",
+            content: "Are the paleo?"
+        }
+      ]
+  },
+  {
+      question: "Would you ever refer to John Harris as Dad?",
+      answers: [
+          {
+              type: "Sarah",
+              content: "Dad has a name?"
+          },
+          {
+              type: "Joey",
+              content: "Do you mean daddy?"
+          },
+          {
+              type: "Mack",
+              content: "Only on a rainy day"
+          },
+          {
+            type: "Brett",
+            content: "{Insert Sign Laugh Here}"
+        },
+        {
+            type: "Becca",
+            content: "NO, NO, NO"
+        },
+      ]
+  },
+  {
+      question: "Do you like who you sit next to in the office?",
+      answers: [
+          {
+              type: "Joey",
+              content: "YESSSSS"
+          },
+          {
+              type: "Mack",
+              content: "You suck Joey"
+          },
+          {
+              type: "Sarah",
+              content: "Sure"
+          },
+          {
+            type: "Brett",
+            content: "Yeah, she keeps all my secrets"
+        },
+        {
+            type: "Becca",
+            content: "Clark Kent is pretty cool"
+        },
+        
+
+      ]
+  },
+//   {
+//       question: "Which of these games do you think is best?",
+//       answers: [
+//           {
+//               type: "Microsoft",
+//               content: "BioShock"
+//           },
+//           {
+//               type: "Nintendo",
+//               content: "The Legend of Zelda: Ocarina of Time"
+//           },
+//           {
+//               type: "Sony",
+//               content: "Final Fantasy VII"
+//           }
+//       ]
+//   },
+//   {
+//       question: "What console would you prefer to own?",
+//       answers: [
+//           {
+//               type: "Microsoft",
+//               content: "X-Box One"
+//           },
+//           {
+//               type: "Nintendo",
+//               content: "Wii U"
+//           },
+//           {
+//               type: "Sony",
+//               content: "Playstation 4"
+//           }
+//       ]
+//   }
+];
+
+export default quizQuestions;
